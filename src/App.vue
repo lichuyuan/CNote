2a8061b359fcc8711cb0acaf36f486f39d7b40a4
李初远 <functionlcy@gmail.com>
2018-05-26 17:30:38 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,65 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <sidebar></sidebar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import sidebar from '@/components/Sidebar.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    sidebar
+  }
 }
 </script>
 
 <style>
-#app {
+@import '//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css';
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html, body, #app {
+  height: 100%;
+}
+
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 14px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #eee;
+}
+
+a {
+  text-decoration: none;
+  color: #444;
+}
+
+ul, li{
+  list-style: none;
+}
+
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background-color: #fff;
+  box-shadow: 0 0 2px 0 #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+
+
+#app {
+  display: flex;
+  align-items: stretch;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import NotebookList from '@/components/NotebookList'
+import NoteDetail from '@/components/NoteDetail'
+import TrashDetail from '@/components/TrashDetail'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      component: NotebookList
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: TrashDetail
     }
   ]
 })
